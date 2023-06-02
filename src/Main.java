46b04b3b45c59fb332b6b5df31ee955d7194510a
benhillerich <hillerbh@email.uc.edu>
2023-06-01 20:24:40 -0400

    Commit(01)

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int numKidsInFam = 13;
+        boolean isRaining = true;
+        double gallonGasPrice = 3.17;
+        int favNum = 13;
+        double shoeSize = 13;
+        int birthMonth = 4;
+        String fullName = "Benjamin Henry Hillerich";
+    }
+}
